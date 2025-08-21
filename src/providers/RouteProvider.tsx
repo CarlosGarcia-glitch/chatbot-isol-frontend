c93aea0232d13f04a837c936997a63f92567aeac
CarlosGarcia <carlos.garcia@isol.dev>
2025-08-21 13:50:26 -0600

    feat: add Account page for change password functionality with styles and routing

--- src/providers/RouteProvider.tsx
+++ src/providers/RouteProvider.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ProtectedRoute } from './ProtectedRoute ';
 import Login from '@/pages/Login/Login';
 import Chat from '@/pages/Chat/Chat';
+import Account from '@/pages/Account/Account';
 
 export const RouterProvider = () => {
   return (
@@ -11,6 +12,7 @@ export const RouterProvider = () => {
 
         <Route element={<ProtectedRoute />}>
           <Route path="/chat" element={<Chat />} />
+          <Route path="/change-password" element={<Account />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/" replace />} />
